167c011d71bd9657563f0e49ee692a2fdd9fffce
N <ardistart@gmail.com>
2018-07-01 06:13:24 +0700

    cek

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package com.sinusoftware.www.bely;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
